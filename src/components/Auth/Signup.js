57937e4f683d31d298f8644d71caa458bb0c07df
Code Scooper <63173572+codescooper@users.noreply.github.com>
2022-04-18 05:40:19 +0200

    preparation des donnée pour creation user

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,4 +1,47 @@
+import React, { useRef, useState } from "react";
+import {useNavigate} from "react-router-dom"
+
 export default function Signup() {
+    const navigate = useNavigate();
+    const [validation, setValidation] = useState("");
+    //Enregistrer les inputs
+    const inputs = useRef([])
+    const addInputs = el => {
+        if(el && !inputs.current.includes(el)){
+        inputs.current.push(el)
+        }
+        console.log(inputs);
+    }  
+
+    // Recuperer les valeurs entré dans le formulaire et les tester 
+    const handleForm = async (e) => {
+        e.preventDefault()
+        if((inputs.current[1].value.length || inputs.current[2].value.length) < 6) {
+          setValidation("6 characters min")
+          return;
+        }
+        else if(inputs.current[1].value !== inputs.current[2].value) {
+          setValidation("Passwords do not match")
+          return;
+        }
+        try {
+          createUser(inputs.current[0].value, inputs.current[1].value);
+          //navigate("/private/private-home")
+        } catch (err) {
+          if(err.code === "auth/invalid-email") {
+            setValidation("Email format invalid")
+          }
+          if(err.code === "auth/email-already-in-use") {
+            setValidation("Email already used")
+          }
+        }
+      }
+
+      const createUser = async (e) => {
+          console.log("creer l'utilisateur : " + inputs.current[0].value);
+          //createUserWithEmailAndPassword(inputs.current[0].value, inputs.current[2].value));
+        }
+
     return (
         <>
             <section className="body-app vh-100">
@@ -9,26 +52,26 @@ export default function Signup() {
                         className="img-fluid" alt="Phone image" />
                     </div>
                     <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-                        <form>
+                        <form onSubmit={handleForm}>
                         <div className="form-outline mb-4">
-                            <input type="email" id="form1Example13" className="form-control form-control-lg" />
+                            <input type="email" ref={addInputs} id="form1Example13" className="form-control form-control-lg" />
                             <label className="form-label" htmlFor="form1Example13">Email address</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                            <input type="password" id="form1Example23" className="form-control form-control-lg" />
+                            <input type="password" ref={addInputs} id="pw1" className="form-control form-control-lg" />
                             <label className="form-label" htmlFor="form1Example23">Password</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                            <input type="password" id="form1Example23" className="form-control form-control-lg" />
+                            <input type="password" ref={addInputs} id="pwd2" className="form-control form-control-lg" />
                             <label className="form-label" htmlFor="form1Example23">Repeat Password</label>
                         </div>
 
                         <div className="d-flex justify-content-around align-items-center mb-4">
 
                             <div className="form-check">
-                            <input className="form-check-input" type="checkbox" value="" id="form1Example3" />
+                            <input className="form-check-input" ref={addInputs} type="checkbox" value="" id="form1Example3" />
                             <label className="form-check-label" htmlFor="form1Example3"> Remember me </label>
                             </div>
                             <a href="#!">Forgot password?</a>
